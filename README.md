af08fe202394e3d65718ac3a897485cd39ae7d17
wenhai <1194630180@qq.com>
2018-12-26 16:05:27 +0800

    测试git diff

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # getTest
-# 这是我的git练习文件夹
+# 这习文件夹
 # 第二次修改
+fdhasfdasl]]d]
+
+dsfjklasdfads
+
+#第三次修改
--- banchmark.rb
+++ banchmark.rb
@@ -1,3 +1,12 @@
 df
+dfsda
 修改了这个以跟踪文件
 ##打发第三方
+###WEqw 
+fsdafjklhl
+ddd
+<script>
+console.log("dsfasdkf")
+
+
+</script>
